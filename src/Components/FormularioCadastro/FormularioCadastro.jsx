429ab4f9f39e4cb925c9a03fea2351a8d0262f4d
joaovictordjardim <joaovictordjardim@gmail.com>
2021-02-02 21:25:23 -0300

    Finalizando gerenciamento do formulário

--- src/Components/FormularioCadastro/FormularioCadastro.jsx
+++ src/Components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, TextField, Switch, FormControlLabel } from '@material-ui/core';
 
 function FormularioCadastro() {
+  const [nome, setNome] = useState('');
+  const [sobrenome, setSobrenome] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [promocoes, setPromocoes] = useState(true);
+  const [novidades, setNovidades] = useState(true);
   return(
-    <form>
+  <form 
+    onSubmit={e => {
+      e.preventDefault();
+      console.log({nome, sobrenome, cpf, promocoes, novidades});
+    }}
+  >
       <TextField 
+        value={nome}
+        onChange={e => setNome(e.target.value)}
         id="nome"
         label="Nome"
         variant="outlined"
@@ -13,6 +25,8 @@ function FormularioCadastro() {
       />
 
       <TextField
+        value={sobrenome}
+        onChange={e => setSobrenome(e.target.value)}
         id="sobrenome"
         label="Sobrenome"
         variant="outlined"
@@ -21,6 +35,13 @@ function FormularioCadastro() {
       />
       
       <TextField
+        value={cpf}
+        onChange={e => {
+          let tmpCpf = e.target.value;
+          if (tmpCpf.length <= 11) {
+            setCpf(tmpCpf);
+          }
+        }}
         id="cpf"
         label="CPF"
         variant="outlined"
@@ -33,9 +54,11 @@ function FormularioCadastro() {
         label="Promoções"
         control={
           <Switch
+            checked={promocoes}
+            onChange={e => setPromocoes(e.target.checked)}
             name="promoções"
             color="primary"
-            defaultChecked
+            // defaultChecked={promocoes}
           />
         }
       />
@@ -44,9 +67,11 @@ function FormularioCadastro() {
         label="Novidades"
         control={
           <Switch
+            checked={novidades}
+            onChange={e => setNovidades(e.target.checked)}
             name="novidades"
             color="primary"
-            defaultChecked
+            // defaultChecked={novidades}
           />
         }
       />
