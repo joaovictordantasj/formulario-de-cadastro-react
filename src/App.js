c31bc3a23228822ee28b210add528623c5dae98f
joaovictordjardim <joaovictordjardim@gmail.com>
2021-02-03 09:19:11 -0300

    Projeto Finalizado

--- src/App.js
+++ src/App.js
@@ -10,9 +10,21 @@ class App extends Component {
     return (
       <Container component="article" maxWidth="sm">
         <Typography variant="h3" component="h1" align="center">Formulário de Cadastro</Typography>
-        <FormularioCadastro />
+        <FormularioCadastro aoEnviar={aoEnviarForm} validarCpf={validarCpf} />
       </Container>
     );
+  }
+}
+
+function aoEnviarForm (dados) {
+  console.log(dados);
+}
+
+function validarCpf (cpf) {
+  if (cpf.length < 11){
+    return {valido:false, texto: 'O CPF deve ter 11 dígitos!'};
+  } else {
+    return {valido:true, texto: ''};
   }
 }
 
--- src/Components/FormularioCadastro/FormularioCadastro.jsx
+++ src/Components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,36 +1,38 @@
 import React, { useState } from 'react';
 import { Button, TextField, Switch, FormControlLabel } from '@material-ui/core';
 
-function FormularioCadastro() {
+function FormularioCadastro({aoEnviar, validarCpf}) {
   const [nome, setNome] = useState('');
   const [sobrenome, setSobrenome] = useState('');
   const [cpf, setCpf] = useState('');
   const [promocoes, setPromocoes] = useState(true);
   const [novidades, setNovidades] = useState(true);
+  const [err, setErr] = useState({cpf:{valido:true, texto:''}})
   return(
   <form 
     onSubmit={e => {
       e.preventDefault();
-      console.log({nome, sobrenome, cpf, promocoes, novidades});
+      aoEnviar({nome, sobrenome, cpf, promocoes, novidades});
+      // console.log({nome, sobrenome, cpf, promocoes, novidades});
     }}
   >
       <TextField 
         value={nome}
         onChange={e => setNome(e.target.value)}
-        id="nome"
-        label="Nome"
-        variant="outlined"
-        margin="normal"
+        id='nome'
+        label='Nome'
+        variant='outlined'
+        margin='normal'
         fullWidth
       />
 
       <TextField
         value={sobrenome}
         onChange={e => setSobrenome(e.target.value)}
-        id="sobrenome"
-        label="Sobrenome"
-        variant="outlined"
-        margin="normal"
+        id='sobrenome'
+        label='Sobrenome'
+        variant='outlined'
+        margin='normal'
         fullWidth
       />
       
@@ -42,41 +44,48 @@ function FormularioCadastro() {
             setCpf(tmpCpf);
           }
         }}
-        id="cpf"
-        label="CPF"
-        variant="outlined"
-        type="number"
-        margin="normal"
+        onBlur={e => {
+          const ehValido = validarCpf(cpf);
+          setErr({cpf:ehValido});
+        }}
+        error={!err.cpf.valido}
+        helperText={err.cpf.texto}
+        id='cpf'
+        label='CPF'
+        variant='outlined'
+        type='number'
+        margin='normal'
         fullWidth
+        required
       />
 
       <FormControlLabel
-        label="Promoções"
+        label='Promoções'
         control={
           <Switch
             checked={promocoes}
             onChange={e => setPromocoes(e.target.checked)}
-            name="promoções"
-            color="primary"
+            name='promoções'
+            color='primary'
             // defaultChecked={promocoes}
           />
         }
       />
 
       <FormControlLabel
-        label="Novidades"
+        label='Novidades'
         control={
           <Switch
             checked={novidades}
             onChange={e => setNovidades(e.target.checked)}
-            name="novidades"
-            color="primary"
+            name='novidades'
+            color='primary'
             // defaultChecked={novidades}
           />
         }
       />
 
-      <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
+      <Button type='submit' variant='contained' color='primary'>Cadastrar</Button>
     </form>
   );
 }
